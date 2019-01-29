5001a2d01b432af25efa3b8b5742aba88eee0c7e
Igawa Hironobu <zin920314@gmail.com>
2019-01-29 18:53:38 +0900

    working

--- webpack.config.js
+++ webpack.config.js
@@ -2,11 +2,14 @@ const path = require('path');
 
 const webpack = require('webpack');
 
+const VueLoaderPlugin = require('vue-loader/lib/plugin');
+const ExtractTextPlugin = require("extract-text-webpack-plugin");
+
 module.exports = (env, args) => {
   const isProduction = args.mode === 'production';
   const isDevelopment = args.mode === 'development';
 
-  const config = {
+  const mainConfig = {
     target: 'electron-main',
     entry: './src/main/main.js',
     output: {
@@ -24,8 +27,47 @@ module.exports = (env, args) => {
     }
   };
 
-  if (isDevelopment)
-    config.devtool = 'source-map';
+  const rendererConfig = {
+    target: 'electron-render',
+    entry: './src/renderer/main.js',
+    output: {
+      path: path.resolve(__dirname, 'dist/renderer'),
+      filename: 'renderer.js'
+    },
+    module: {
+      rules: [
+        {
+          test: /\.vue$/,
+          use: 'vue-loader',
+        },
+        {
+          test: /\.js$/,
+          exclude: /node_modules/,
+          use: "babel-loader"
+        },
+        {
+          test: /\.css$/,
+          use: ExtractTextPlugin.extract({
+            fallback: "style-loader",
+            use: `css-loader${isProduction ? "?minimize" : ""}`
+          })
+        }
+      ]
+    },
+    resolve: {
+      alias: {
+        "vue$": "vue/dist/vue.esm.js"
+      },
+      extensions: [".vue", ".js"]
+    },
+    plugins: [
+      new VueLoaderPlugin(),
+      new ExtractTextPlugin("main.css")
+    ]
+  };
 
-  return config;
+  if (isDevelopment)
+    mainConfig.devtool = 'source-map';
+
+  return mainConfig;
 };
